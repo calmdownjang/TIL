ebd7a27459ca4e67740795a02486f41828e75d3e
calmdownjang <derek040404@gmail.com>
2023-01-21 22:09:29 +0900

    start

---
+++ 1.py/2.py
@@ -0,0 +1,64 @@
+# a=int(input("태어난 년도를 입력하세요. > "))
+# print("내 나이:",2024-a)
+
+
+
+# a=int(input("첫 번쨰 정수"))
+# b=int(input("두 번째 정수"))
+# if a < b :
+# print(b-a)
+# if a>b:
+# print(a-b)
+
+
+
+# z1=[1,23,543,6,437,744]
+# z2=[31,4124,12,32,5431]
+# z3=z1+z2
+# z3.sort()
+# print(z3)
+
+
+
+# dic={'name':'derek', 'age':'20'}
+# print(dic['name'])
+# key가 네임 에이지 key 가 중복되면 x
+
+
+
+
+
+# a={1:'장연우', 2:'임지혜', 3:'임성주'}
+# # print(a.keys())
+# # print(a.values())
+# # print(a.items())
+
+# # for k in a.items():
+# # print(k)
+
+# print(a[4]) #? KeyError: 4
+# print(a.get(4)) #? None
+# print(a.get(4,'없음')) #? 없음
+# print( 4 in a) #? False
+
+
+
+
+
+# s1=set([1,2,3,4])
+# s1.update({6,7,8})
+# s1.remove(2)
+# # print(s1)
+
+
+
+
+# a=[1,2,3,4]
+# while a:
+# a.pop()
+# print(a)
+# ?
+# [1, 2, 3]
+# [1, 2]
+# [1]
+# []
